d57c5c54c2c437a824eec18ff531e5105cd17843
blessing999 <111128074+blessing999@users.noreply.github.com>
2022-09-14 15:59:47 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
 /**
- *main - check for multiples of 3 and 5
+ * main - Entry point
  *
- *Return: 0 always
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int x = 1024, y, sum = 0;
-for (y = 0; y < x; y++)
-{
-if ((y % 3 == 0) || (y % 5 == 0))
-{
-sum = sum + y;
-}
-}
-printf("%d\n", sum);
-return (0);
+
+	return (0);
 }
