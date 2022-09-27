827ad772e5e069e4ee7dbf9ea7129ba7abd0d063
blessing999 <blessinguzor7@gmail.com>
2022-09-27 07:22:03 -0700

    My first work

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * print_chessboard - print a chessboard.
  * @a: source matrix
@@ -18,3 +18,4 @@ void print_chessboard(char (*a)[8])
 		}
 		_putchar('\n');
 	}
+}
